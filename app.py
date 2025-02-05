10eeeb4e222740652bb8b1f68ab9c5dcac936a1d
shashikumar reddy <55330284+shashikumar04433@users.noreply.github.com>
2025-02-05 15:32:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def api():
+    return jsonify({"message": "Hello from Flask API!"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
